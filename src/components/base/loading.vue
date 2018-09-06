9d412396487ac972a1c68b022604752b7fab7cb1
邓寅 <dengyin@beautyyan.cn>
2018-09-06 18:09:29 +0800

    test

---
+++ src/components/base/loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loading">
+    <img src="../../common/images/lazy.gif" alt="">
+    <p>{{title}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '正在载入...'
+
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .loading
+    text-align center
+</style>
+
+
